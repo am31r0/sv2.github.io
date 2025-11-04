49f51ab3913f76e6afc3ba59cc4c5e4ce2cb39de
am31r0 <crizybeats@gmail.com>
2025-11-04 13:59:26 +0100

    new update

--- src/lib/storeSelector.js
+++ src/lib/storeSelector.js
@@ -9,27 +9,27 @@ export function renderStoreSelector(mount) {
     <div class="store-select-group">
       <label class="store-option selector-img ah" title="Albert Heijn">
         <input type="checkbox" data-store="ah" />
-        <img src="./public/icons/ah-logo.webp" alt="Albert Heijn" />
+        <img src="icons/ah-logo.webp" alt="Albert Heijn" />
       </label>
 
       <label class="store-option selector-img dirk" title="Dirk">
         <input type="checkbox" data-store="dirk" />
-        <img src="./public/icons/dirk-logo-square.webp" alt="Dirk" />
+        <img src="icons/dirk-logo-square.webp" alt="Dirk" />
       </label>
 
       <label class="store-option selector-img jumbo" title="Jumbo">
         <input type="checkbox" data-store="jumbo" />
-        <img src="./public/icons/jumbo-logo-yellow-square.webp" alt="Jumbo" />
+        <img src="icons/jumbo-logo-yellow-square.webp" alt="Jumbo" />
       </label>
 
       <label class="store-option selector-img aldi" title="Aldi">
         <input type="checkbox" data-store="aldi" />
-        <img src="./public/icons/aldi-logo.webp" alt="Aldi" />
+        <img src="icons/aldi-logo.webp" alt="Aldi" />
       </label>
 
       <label class="store-option selector-img hoogvliet" title="hoogvliet">
         <input type="checkbox" data-store="hoogvliet" />
-        <img src="./public/icons/hoogvliet-logo.webp" alt="hoogvliet" />
+        <img src="icons/hoogvliet-logo.webp" alt="hoogvliet" />
       </label>
     </div>
   `;
